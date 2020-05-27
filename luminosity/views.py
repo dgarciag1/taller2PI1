7ac7c66142882c57bedabd63503b5fe92b957174
dgarciag1 <dgarciag@eafit.edu.co>
2020-05-27 17:20:46 -0500

    correccion de link

--- luminosity/views.py
+++ luminosity/views.py
@@ -8,9 +8,9 @@ def luminosity(request):
             if value:
 
                 args = {'type': 'lux', 'value': value}
-                response = requests.post('https://pi1-eafit-dgarciag-fr.azurewebsites.net/luminositys/', args)
+                response = requests.post('https://pi1-eafit-dgarciag.azurewebsites.net/luminositys/', args)
                 luminosity_json = response.json()
 
-    response = requests.get('https://pi1-eafit-dgarciag-fr.azurewebsites.net/luminositys/')
+    response = requests.get('https://pi1-eafit-dgarciag.azurewebsites.net/luminositys/')
     luminositys = response.json()
     return render(request, "luminosity.html", {'luminositys': luminositys})
