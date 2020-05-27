697f8dc3a85505250ec43b0e2e979a248e33eab4
dgarciag1 <dgarciag@eafit.edu.co>
2020-05-27 18:15:10 -0500

    correcion pagina

--- luminosity/views.py
+++ luminosity/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render, HttpResponse
 import requests
 # Create your views here.
+
 def luminosity(request):
     if 'value' in request.GET:
-            value = request.GET['value']
+       value = request.GET['value']
 
-            if value:
-
-                args = {'type': 'lux', 'value': value}
-                response = requests.post('https://pi1-eafit-dgarciag.azurewebsites.net/luminositys/', args)
-                luminosity_json = response.json()
+       if value:
+           args = {'type': 'lux', 'value': value}
+           response = requests.post('https://pi1-eafit-dgarciag.azurewebsites.net/luminositys/', args)
+           luminosity_json = response.json()
 
     response = requests.get('https://pi1-eafit-dgarciag.azurewebsites.net/luminositys/')
     luminositys = response.json()
